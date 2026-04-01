d0cce847e6c9c425623b6775225f8423abf1cd13
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-04-01 01:05:43 +0000

    Implementacion de la secuencia dada por el usuario

--- main.c
+++ main.c
@@ -2,7 +2,24 @@
 
 int main() {
   int numero_inicial;
-  int numero_fina;
+  int numero_final;
   int incremento = 0;
+
+  printf("%s", "Ingrese el numero inicial");
+  scanf("%d", &numero_inicial);
+
+  printf("%s", "Inicie el numero final");
+  scanf("%d", &numero_final);
+
+  printf("%s", "Ingrese el valor de incremento de la secuecia");
+  scanf("%d", &incremento);
+
+  for (int i = numero_inicial; i <= numero_final; i += incremento) {
+    printf("%d", i);
+  }
+
+  // for (int i = 0; i <= 10; i++) {
+  // printf("%d\n", i);
+  // }
   return 0;
 }
